83ef690ef29c41cf67c6607443dd8028b746158d
hanbinsite <1837261009@qq.com>
2024-09-20 00:02:44 +0800

    新增分页部分

--- src/test/java/site/hanbin/response/common/ResponseCommonApplicationTests.java
+++ src/test/java/site/hanbin/response/common/ResponseCommonApplicationTests.java
@@ -1,9 +1,15 @@
 package site.hanbin.response.common;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import site.hanbin.response.common.page.model.PageConfig;
+import site.hanbin.response.common.page.utils.PageUtils;
 import site.hanbin.response.common.result.utils.ResultUtils;
+
+import java.util.List;
 
 @SpringBootTest
 @Slf4j
@@ -17,6 +23,10 @@ class ResponseCommonApplicationTests {
         log.info("成功  自定义数据: {}", ResultUtils.success("这是数据"));
         log.info("异常 自定义 状态码， 信息: {}", ResultUtils.error(1, "返回信息"));
         log.info("异常 自定义 信息: {}", ResultUtils.success("返回信息"));
+
+
+
+
     }
 
 }
