db2735943e9bbea26cfdba0b16d5f0bdd91da5f0
hanbin <1837261009@qq.com>
2024-12-18 10:41:57 +0800

    修改代码配置

--- src/main/java/site/hanbin/response/common/result/utils/ResultUtils.java
+++ src/main/java/site/hanbin/response/common/result/utils/ResultUtils.java
@@ -1,7 +1,7 @@
 package site.hanbin.response.common.result.utils;
 
 
-import site.hanbin.response.common.result.enums.ResultEnum;
+import site.hanbin.response.common.result.enums.CustomEnum;
 import site.hanbin.response.common.result.model.Result;
 
 /**
@@ -11,6 +11,11 @@ public class ResultUtils {
 
     /**
      * 成功时生成result的方法,有返回数据
+     * @param code 状态码
+     * @param msg 信息
+     * @param t 数据
+     * @return 任意数据
+     * @param <T> 任意数据
      */
     public static <T> Result<T> success(Integer code, String msg, T t) {
         Result<T> result = new Result<>();
@@ -23,27 +28,39 @@ public class ResultUtils {
 
     /**
      * 成功时生成result的方法,无返回数据
+     * @return 数据
+     * @param <T> 任意数据
      */
     public static <T> Result<T> success() {
-        return success(ResultEnum.SUCCESS.getCode(), ResultEnum.SUCCESS.getMsg(), null);
+        return success(CustomEnum.CustomCodeEnum.SUCCESS.getCode(), CustomEnum.CustomCodeEnum.SUCCESS.getMsg(), null);
     }
 
     /**
      * 成功时生成result的方法,无返回数据
+     * @param msg 返回信息
+     * @return 返回数据
+     * @param <T> 任意数据
      */
     public static <T> Result<T> successOnlyMsg(String msg) {
-        return success(ResultEnum.SUCCESS.getCode(), msg, null);
+        return success(CustomEnum.CustomCodeEnum.SUCCESS.getCode(), msg, null);
     }
 
     /**
      * 成功时生成result的方法,无返回数据
+     * @param t 返回数据
+     * @return 返回数据
+     * @param <T> 任意数据
      */
     public static <T> Result<T> success(T t) {
-        return success(ResultEnum.SUCCESS.getCode(), ResultEnum.SUCCESS.getMsg(), t);
+        return success(CustomEnum.CustomCodeEnum.SUCCESS.getCode(), CustomEnum.CustomCodeEnum.SUCCESS.getMsg(), t);
     }
 
     /**
      * 失败时生成result的方法
+     * @param status 状态码
+     * @param msg 返回信息
+     * @return 任意数据
+     * @param <T> 返回数据
      */
     public static <T> Result<T> error(int status, String msg) {
         Result<T> result = new Result<>();
@@ -53,10 +70,32 @@ public class ResultUtils {
         return result;
     }
 
-    /**
+    /**\
      * 失败时生成result的方法
+     * @param msg 返回信息
+     * @return 返回数据
+     * @param <T> 任意数据
      */
     public static <T> Result<T> error(String msg) {
-        return error(ResultEnum.FAIL.getCode(), msg);
+        return error(CustomEnum.CustomCodeEnum.FAIL.getCode(), msg);
+    }
+
+    /**
+     * 返回未知错误异常
+     * @param msg 返回信息
+     * @return 返回信息
+     * @param <T> 任意数据
+     */
+    public static <T> Result<T> unknown(String msg) {
+        return error(CustomEnum.CustomCodeEnum.UNKNOWN_ERROR.getCode(), msg);
+    }
+
+    /**
+     * 返回未知错误异常
+     * @return 返回信息
+     * @param <T> 任意数据
+     */
+    public static <T> Result<T> unknown() {
+        return error(CustomEnum.CustomCodeEnum.UNKNOWN_ERROR.getCode(), CustomEnum.CustomCodeEnum.UNKNOWN_ERROR.getMsg());
     }
 }
